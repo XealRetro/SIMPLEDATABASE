d8a62328fbace48b02458f079bd29036dd768442
XealRetro <megalodonxgaming@gmail.com>
2016-05-20 11:59:54 -0400

    Update Main.cpp
    Added Menu and queue

--- SimpleDatabase/SimpleDatabase/Main.cpp
+++ SimpleDatabase/SimpleDatabase/Main.cpp
@@ -7,17 +7,58 @@ int main(void)
 {
 	MiniDataBase test;
 	srand(time(NULL));
-	int count = 0;
-	//test.countRecords();
+	int selection[12];
+
+	cout << "**********SIMPLE DATABASE MENU**********" << endl;
+	cout << "1. Add Record" << endl;
+	cout << "2. Delete Record" << endl;
+	cout << "3. Search Records" << endl;
+	cout << "4. Display Records" << endl;
+	cout << "5. Index Records" << endl;
+	cout << "Please enter your selections into the queue (maximum of 12)" << endl;
+	for (int w = 0; w < 12; w++)
+	{
+		cout << "Press enter after every selection" << endl;
+		cin >> selection[w];
+		while (selection[w] > 5 || selection[w] < 1)
+		{
+			cout << "Incorrect selection. Please try again!" << endl;
+			cin >> selection[w];
+		}
+	}
+
+	for (int e = 0; e < 12; e++)
+	{
+		if (selection[e] == 1)
+		{
+			test.getInput();
+		}
+		if (selection[e] == 2)
+		{
+			test.deleteRecord();
+		}
+		if (selection[e] == 3)
+		{
+			test.search();
+		}
+		if (selection[e] == 4)
+		{
+			test.displayRecords();
+		}
+		if (selection[e] == 5)
+		{
+			test.indexRecords();
+		}
+	}
 
 	//test.getInput();
-		//count++;
-
 	//test.search();
-	test.indexRecords();
+	//test.indexRecords();
+	//cout << "\n\n";
+	//test.displayRecords();
 	//test.deleteRecord();
-	cout << "Done\n";
+	cout << "Done" << endl;;
 	getchar();
-	getchar();
+
 	return 0;
 }
